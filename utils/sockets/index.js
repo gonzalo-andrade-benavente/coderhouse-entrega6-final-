06c068572f9c09045bac9501604bf12d67c6805f
Gonzalo Andrade <gonzaloandrade@MacBook-Pro-de-equipamiento.local>
2021-12-08 19:32:25 -0300

    Final

--- utils/sockets/index.js
+++ utils/sockets/index.js
@@ -11,23 +11,49 @@ class Socket {
 
         Socket.instancia = this;
         this.io = new Server(http);
-        this.mensajes = [];
-        this.usuarios = [];
+        this.messages = [];
+        this.users = [];
         this.products = [];
     }
 
     init() {
-        this.io.on('connection', (socket) => {
 
-            socket.emit('init', this.products);
+        try {
 
-            socket.on('addProduct', data => {
-                this.products.push(data);
-                this.io.sockets.emit('listenserver', this.products);
+            this.io.on('connection', (socket) => {
+
+                const dateConnectd = new Date();
+                
+                console.log(`User connected at ${dateConnectd}`);
+
+                socket.emit('init', this.products);
+    
+                socket.on('addProduct', data => {
+                    console.log(`addProduct`);
+                    this.products.push(data);
+                    this.io.sockets.emit('listenserver', this.products);
+                });
+
+                socket.on('addUser', user => {
+                    console.log(`addUser`);
+                    this.users.push(user);
+                    this.io.sockets.emit('listenServerMessages', this.messages);
+                })
+
+                socket.on('addMessage', message => {
+                    console.log(`addMessage`, message);
+                    this.messages.push(message);
+                    this.io.sockets.emit('listenServerMessages', this.messages);
+                })
+    
+    
             });
 
-
-        });
+        }
+        catch (err) {
+            console.log(err);
+        }
+        
     }
 
 }
